a79c43dc266210395d5ff968ae75f2f391092f88
fabrint96-ship-it <fabrint96@gmail.com>
2026-05-08 23:28:19 +0200

    Delete hash.php

--- hash.php
+++
@@ -1,8 +0,0 @@
-<?php
-echo "GENERADOR DE CONTRASENAS CON HASH";
-echo "admin123:";
-echo password_hash("admin123", PASSWORD_DEFAULT);
-
-echo "tecnico1123:";
-echo password_hash("admin123", PASSWORD_DEFAULT);
-?>
