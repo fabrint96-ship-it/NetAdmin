00f9ab92be7b02561f4751dccb1c51beac0b74f0
Fabricio Torres <fernandoprimero22@gmail.com>
2026-05-16 02:12:49 +0200

    Sistema real de backups PostgreSQL mejorado

--- backups.php
+++ backups.php
@@ -3,10 +3,7 @@ require_once 'includes/auth.php';
 require_once __DIR__ . '/config/db.php';
 require_once 'includes/functions.php';
 
-if (!isset($_SESSION['rol']) || $_SESSION['rol'] !== 'admin') {
-
-    die("Acceso denegado");
-}
+bloquearSiNoEsAdmin();
 
 $backups = $pdo->query("
     SELECT *
